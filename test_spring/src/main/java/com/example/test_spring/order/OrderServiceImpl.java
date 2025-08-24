6c22313dec5ebd8a826679d07ae38cf741db0206
202110861 <dbsalsk2514@naver.com>
2025-08-24 13:20:28 +0900

    의존관계 주입 방법 4가지

--- test_spring/src/main/java/com/example/test_spring/order/OrderServiceImpl.java
+++ test_spring/src/main/java/com/example/test_spring/order/OrderServiceImpl.java
@@ -10,12 +10,25 @@ import org.springframework.stereotype.Component;
 public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+    //@Autowired private DiscountPolicy discountPolicy   //의존관계 자동 주입 방법 3 - 필드 주입
 
-    @Autowired //자동으로 의존성관계 주입
+
+    @Autowired //의존관계 자동 주입 방법 1 - 생성자 주입
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+//
+//    @Autowired //의존관계 자동 주입 방법 2 - 수정자 주입(setter 주입)
+//    public void setMemberRepository(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+//
+//    @Autowired
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy=discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
--- test_spring/src/test/java/com/example/test_spring/order/OrderServiceTest.java
+++ test_spring/src/test/java/com/example/test_spring/order/OrderServiceTest.java
@@ -24,4 +24,11 @@ public class OrderServiceTest {
         Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
 
     }
+
+    @Test
+    void fieldInjectionTest(){
+        //OrderService orderService = new OrderServiceImpl();
+        orderService.createOrder(1L, "itemA", 10000);
+
+    }
 }
